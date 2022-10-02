c600114c95d1ba0aa0df0869d0fba5c305ee4878
khanshorif331 <khanshorif331@gmail.com>
2022-10-03 00:39:56 +0600

    programme done

--- main.cpp
+++ main.cpp
@@ -3,15 +3,37 @@ using namespace std;
 
 int main()
 {
-    int x = 1;
-    int number;
-    int total = 0;
-    while (x <= 5)
+
+    // ---------------get the average of the user inputs of age----------------------
+
+    int age;
+    int ageTotal = 0;
+    int numberOfPeople = 0;
+
+    cout << "Enter first persons age or -1 to quit: " << endl;
+
+    cin >> age;
+
+    while (age != -1)
     {
-        cin >> number;
-        total += number;
-        x++;
+        ageTotal = ageTotal + age;
+        numberOfPeople++;
+        cout << "Enter next persons age or -1 to quit: " << endl;
+        cin >> age;
     }
-    cout << "The sum is " << total << endl;
+    cout << "The total is " << ageTotal << numberOfPeople << endl;
+
+    // -----------------------program to take input frm user and get the sum--------------------------
+
+    // int x = 1;
+    // int number;
+    // int total = 0;
+    // while (x <= 5)
+    // {
+    //     cin >> number;
+    //     total += number;
+    //     x++;
+    // }
+    // cout << "The sum is " << total << endl;
     return 0;
 }
